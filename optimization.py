49e2a6df5fc7116cfd8bceab44510ac0eafa3a6d
Lanke Fu <lf2869@torch-login-a-2.hpc-infra.svc.cluster.local>
2026-07-22 20:45:56 -0400

    initial commit

---
+++ optimization.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # NAE optimization
+# This file sees how far we can push pyAQSC's aspect ratio
+
+# In[1]:
+import os
+import aqsc
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import jax
+import jax.numpy as jnp
+jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
+jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
+jax.config.update("jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir")
+from pathlib import Path
+from shared import *
+from jax import flatten_util, jit, jacfwd
+from functools import partial
+from jax.lax import while_loop
+
+
+
+# In[4]:
+
+
+target_iota = 0.2
+target_anisotropy = 1e-2
+target_aspect = 10.
+target_beta = 0.05
+population_size = 200
+w_aspect, w_anisotropy, w_iota, w_p = 6., 0.5, 1., 1.
+
+
+def objective(x_flat, w_aspect, w_anisotropy, w_iota, w_p, m, full_mode=False):
+    in_dict = unravel_x(x_flat)
+    eq = solve_order_6(x_flat, m, padded=False)
+    anisotropy = rms_anisotropy(eq)
+    
+    psi_crit, _, _ = eq.get_psi_crit()
+    eps_crit_val = jnp.sqrt(psi_crit)
+    eps_conv_val = eps_conv(eq)
+    eps = jnp.minimum(eps_conv_val, eps_crit_val)
+    aspect = eq.aspect_ratio_eps(eps)
+
+    # Iota
+    iota_a = jnp.real(eq.iota.eval(psi=0, chi=0, phi=0))
+
+    # Effective field strength at the edge
+    B_denom_20 = jnp.real(in_dict['B20'])
+    B_denom_0 = jnp.real(in_dict['B0'])
+    B_denom_edge_eff = (B_denom_0 + B_denom_20*eps**2)
+    # Effective pressure at the edge
+    p20_avg = jnp.real(jnp.average(eq.p_perp[2][0].content))
+    p00_avg = jnp.real(jnp.average(eq.p_perp[0][0].content))
+    p_edge_eff = (p00_avg + p20_avg*eps**2)
+    p_axis_eff = p00_avg
+    # # beta_edge eff
+    # beta_axis_eff = p00_avg*B_denom_0
+    # beta_edge_eff = p_edge_eff*B_denom_edge_eff
+    B2_eff = 1/(B_denom_0 + B_denom_20 * eps**2)
+    term1 = w_aspect * (
+        jnp.maximum(aspect - target_aspect, 0) / target_aspect
+    )**2
+    term2 = w_anisotropy * (
+        jnp.maximum(anisotropy - target_anisotropy, 0) / target_anisotropy
+    )**2
+    term3 = w_iota * (
+        jnp.maximum(jnp.abs(target_iota) - jnp.abs(iota_a), 0) / target_iota
+    )**2
+    # term4 = w_p * (
+    #     jnp.maximum(p20_avg, 0) / p20_avg
+    # )**2
+    term4 = w_p * (
+        jnp.maximum(p_edge_eff - p_axis_eff, 0) / p_axis_eff
+    )**2
+    out = term1 + term2 + term3 + term4
+    if full_mode:
+        return {
+            'eps_crit': eps_crit_val,
+            'eps_conv': eps_conv_val,
+            'aspect': aspect,
+            'anisotropy': anisotropy,
+            'p_edge_eff': p_edge_eff,
+            'p_axis_eff': p_axis_eff,
+            'iota_a': iota_a,
+        }
+            
+    return out
+
+def obj_wrapped(x_flat, w_aspect, w_anisotropy, w_iota, w_p, m):
+    out = objective(x_flat, w_aspect, w_anisotropy, w_iota, w_p, m)
+    if np.isfinite(out):
+        return out
+    else:
+        return 1e10
+
+def _objective_vmap(x, a, b, c, d):
+    return jax.vmap(
+        lambda x_, a_, b_, c_, d_: objective(x_, a_, b_, c_, d_, 10),
+        in_axes=(0, None, None, None, None),
+        out_axes=0,
+    )(x, a, b, c, d)
+
+def _objective_vmap_full(x, a, b, c, d):
+    return jax.vmap(
+        lambda x_, a_, b_, c_, d_: objective(x_, a_, b_, c_, d_, 10, full_mode=True),
+        in_axes=(0, None, None, None, None),
+        out_axes=0,
+    )(x, a, b, c, d)
+
+objective_vmap = jax.jit(_objective_vmap)
+objective_vmap_full = jax.jit(_objective_vmap_full)
+
+import jax
+from evosax.algorithms import CMA_ES
+import time
+import numpy as np
+
+path = ''
+if os.path.exists("best_x.npy"):
+    x_flat_init = jnp.array(np.load("best_x.npy"))
+    path = 'pass_2/'
+    Path(path).mkdir(parents=True, exist_ok=True)
+    Path(path+'global_steps').mkdir(parents=True, exist_ok=True)
+    Path(path+'population_eq').mkdir(parents=True, exist_ok=True)
+    print('*** restarting ***')
+    restart_stats = objective(x_flat_init, w_aspect, w_anisotropy, w_iota, w_p, 10, full_mode=True)
+    print(restart_stats)
+
+# Instantiate the search strategy
+es = CMA_ES(population_size=population_size, solution=x_flat_init)
+# The original initial std is 1.0. Increase it to hopefully find a better sln
+params = es.default_params.replace(std_init=5.0)
+
+# Initialize state
+key = jax.random.key(0)
+state = es.init(key, x_flat_init, params)
+key, key_ask, key_eval = jax.random.split(key, 3)
+# Generate a set of candidate solutions to evaluate
+population, state = es.ask(key_ask, state, params)
+
+fitness1 = objective_vmap(population, w_aspect, w_anisotropy, w_iota, w_p)
+num_generations = 2000
+fitness_list = []
+i_list = []
+time_tot = 0
+ask = jit(es.ask)
+last_avg = np.inf
+
+# Ask-Eval-Tell loop
+time_init = time.time()
+for i in range(num_generations):
+    key, key_ask, key_eval = jax.random.split(key, 3)
+
+    # Generate a set of candidate solutions to evaluate
+    population, state = ask(key_ask, state, params)
+
+    # Actually fits in memory
+    time1 = time.time()
+    fitness = objective_vmap(population, w_aspect, w_anisotropy, w_iota, w_p)
+    time2 = time.time()
+    
+    # Update the evolution strategy
+    state, metrics = es.tell(key, population, fitness, state, params)
+    
+    if i%10==0:
+        data_dict = objective_vmap_full(population, w_aspect, w_anisotropy, w_iota, w_p)
+        fitness_list.append(fitness.copy())
+        i_list.append(i)
+        fit_min = jnp.nanmin(fitness)
+        fit_avg = jnp.nanmean(fitness)
+        print(
+            'i', i,
+            'time', time2-time1, 
+            'eps_crit', jnp.nanmin(data_dict['eps_crit']), jnp.nanmax(data_dict['eps_crit']), 
+            'eps_conv', jnp.nanmin(data_dict['eps_conv']), jnp.nanmax(data_dict['eps_conv'])
+        )
+        print('min', fit_min, 'mean', fit_avg)
+        print('#nan', np.sum(np.isnan(fitness)))
+        jnp.save(path + 'global_steps/step'+str(i), {
+            'population': population,
+            'fitness': fitness,
+            'data_dict': data_dict,
+        })
+        if fit_min < 2:
+            print('Good enough')
+            break
+        
+        if jnp.abs(fit_avg - last_avg) < 1:
+            print('Converged')
+            break
+        last_avg = fit_avg
+    
+time_fin = time.time()
+time_tot = time_tot + (time_fin - time_init)
+print('total time', time_fin - time_init)
+# Get best solution
+
+
+# In[95]:
+
+
+argsort_fitness = np.argsort(fitness)
+sorted_fitness = fitness[argsort_fitness]
+sorted_population = population[argsort_fitness]
+
+
+# In[71]:
+
+
+jnp.save(path + 'population.npy', population)
+jnp.save(path + 'fitness.npy', fitness)
+jnp.save(path + 'fitness_list.npy', fitness_list)
+jnp.save(path + 'time_tot.npy', time_tot)
+
+best_i = np.nanargmin(fitness)
+best_x = population[best_i]
+jnp.save(path + 'best_x.npy', best_x)
+
+
+# In[96]:
+
+
+plt.plot(fitness[argsort_fitness])
+plt.yscale('log')
+
+
+# In[41]:
+
+
+objective_vmap(sorted_population[:16])
+
+
+# In[43]:
+
+
+for i in range(len(sorted_fitness)):
+    if sorted_fitness[i]:
+        eq_fin = solve_order_6(jnp.array(sorted_population[i]), padded=False)
+        fin_anisotropy = rms_anisotropy(eq_fin)
+        fin_aspect = aspect_conv(eq_fin)
+        fin_eps_conv = eps_conv(eq_fin)
+        fin_iota = iota_axis(eq_fin)
+        p20_avg = jnp.real(jnp.average(eq_fin.p_perp[2][0].content))
+        p00_avg = jnp.real(jnp.average(eq_fin.p_perp[0][0].content))
+        p_edge_eff = (p00_avg + p20_avg*eps**2)
+        p_axis_eff = p00_avg
+        print(
+            'Δ:', f'{fin_anisotropy:>12.4e}', '    '
+            'R/r:', f'{fin_aspect:>12.4e}', '    '
+            'ι0:', f'{fin_iota:>12.4e}', '    '
+            'p_axis-p_edge (estimate):', f'{p_axis_eff - p_edge_eff:>12.4e}', '    '
+        )
+        pres_profile = eq_fin.p_perp.eval_eps(jnp.linspace(0, fin_eps_conv, 30), 0, 0)
+        plt.plot(pres_profile/jnp.max(pres_profile))
+        if fin_anisotropy and fin_aspect and fin_iota:
+            jnp.save(path + 'population_eq/eq'+str(i), eq_fin)
+
---
+++ shared.py
@@ -0,0 +1,261 @@
+import importlib
+import aqsc
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import jax
+import jax.numpy as jnp
+
+jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
+jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
+jax.config.update("jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir")
+
+from jax import flatten_util, jit, jacfwd
+from functools import partial
+from jax.lax import while_loop
+
+# Constants and resolution
+static_freq = 100 # low-pass filter freq
+traced_freq = 40 # low-pass filter freq
+len_phi = 100 # number of phi sample points
+phis_2pi = jnp.linspace(0,2*jnp.pi, len_phi, endpoint=False)
+nfp = 2
+R0_fixed = 1.0 # fixed major radius
+RZ_clip = 0.3 # fixing min and max R and Z coeffs to prevent crazy axes
+
+def spec_weight(n):
+    return jnp.e**(-jnp.arange(n))
+
+Rc = jnp.array([1.,   0.1,   0., 0., 0., 0., 0., 0.])
+Zs = jnp.array([0.,   0.1,   0., 0., 0., 0., 0., 0.])
+pc = jnp.array([0.05, 0.005, 0., 0., 0., 0., 0., 0.])
+
+# Spectral weighting to make optimization converge better
+Rc_raw = Rc/spec_weight(len(Rc))
+Zs_raw = Zs/spec_weight(len(Zs))
+pc_raw = pc/spec_weight(len(pc))
+
+# Initial cond and unraveling
+init_dict = {
+    'Rc_raw': Rc_raw,
+    'Zs_raw': Zs_raw,
+    # 'Rc': qa_Rc,
+    # 'Zs': qa_Zs,
+    'pc_raw': pc_raw,
+    'Delta_0_avg': 0.,
+    'B_theta_20_avg': 0.,
+    'B_alpha_1': 0.1,
+    'B0': 1.0,
+    'B11c': -1.8,
+    'B22c': 0.,
+    'B20': 0., 
+    'B22s': 0.,
+    'B_alpha_nb2_34': 0.,
+    'B_alpha_nb2_56': 0.,
+    'B_denom_3': [0., 0.], # 0., 0.], # B33s, B31s, B31c, B33c
+    'B_denom_4': [0.,], # 0., 0., 0., 0.], # B33s, B31s, B31c, B33c
+    'B_denom_5': [0., 0.,], # 0., 0., 0., 0.], # B33s, B31s, B31c, B33c
+    'B_denom_6': [0.,], # 0., 0., 0., 0., 0., 0.], # B33s, B31s, B31c, B33c
+}
+
+x_flat_init, unravel_x = flatten_util.ravel_pytree(init_dict)
+
+@partial(jit, static_argnames='padded')
+def solve_order_6(x_flat, m=10, padded=True):
+    in_dict = unravel_x(x_flat)
+    
+    Rc_raw = in_dict['Rc_raw']
+    Zs_raw = in_dict['Zs_raw']
+    pc_raw = in_dict['pc_raw']
+    
+    # Clipping the fourier coefficients
+    # to prevent crazy axis
+    Rc_raw = jnp.clip(Rc_raw, -RZ_clip, RZ_clip)
+    Zs_raw = jnp.clip(Zs_raw, -RZ_clip, RZ_clip)
+
+    if padded:
+        chiphifunc_type = aqsc.ChiPhiFuncPadded
+    else:
+        chiphifunc_type = aqsc.ChiPhiFunc
+    
+    # Weighting Fourier coeffs of pressure and axis shape
+    weight_pc = spec_weight(len(pc_raw))
+    weight_RZ = spec_weight(len(Rc_raw))
+    
+    pc_raw = pc_raw * weight_pc
+    Rc_raw = Rc_raw * weight_RZ
+    Zs_raw = Zs_raw * weight_RZ
+    
+    m_list = jnp.arange(len(in_dict['Rc_raw']))
+    pc_raw = jnp.where(m_list<=m, pc_raw, 0)
+    Rc_raw = jnp.where(m_list<=m, Rc_raw, 0)
+    Zs_raw = jnp.where(m_list<=m, Zs_raw, 0)
+    
+    pc = pc_raw
+    Rc = list(Rc_raw)
+    Zs = list(Zs_raw)
+    
+    # Fixing major radius
+    Rc[0] = R0_fixed
+    
+    # Stellsym axis
+    Rs = [0] * len(Rc)
+    Zc = [0] * len(Zs)
+
+    Delta_0_avg = in_dict['Delta_0_avg']
+    B_theta_20_avg = in_dict['B_theta_20_avg']
+    B_alpha_1 = in_dict['B_alpha_1']
+    B0 = in_dict['B0']
+    B11c = in_dict['B11c']
+    B22c = in_dict['B22c']
+    B20 = in_dict['B20']
+    B22s = in_dict['B22s']
+    B_alpha_nb2_34 = in_dict['B_alpha_nb2_34']
+    B_alpha_nb2_56 = in_dict['B_alpha_nb2_56']
+    B_denom_3 = in_dict['B_denom_3']
+    B_denom_4 = in_dict['B_denom_4']
+    B_denom_5 = in_dict['B_denom_5']
+    B_denom_6 = in_dict['B_denom_6']
+    # Reconstructing the p array from cos coeffs
+    p_mode_num = jnp.arange(len(pc))*nfp
+    p_Phi0 = phis_2pi/nfp
+    p_phi_times_mode = p_mode_num[:, None]*p_Phi0[None, :]
+    p_cos_arr = jnp.cos(p_phi_times_mode)
+    p0 = jnp.sum(pc[:, None]*p_cos_arr, axis=0)
+    equilibrium = aqsc.leading_orders(
+        nfp=nfp,
+        Rc=Rc,
+        Rs=Rs,
+        Zc=Zc,
+        Zs=Zs,
+        p0=p0,
+        Delta_0_avg=Delta_0_avg,
+        B_alpha_1=B_alpha_1,
+        B0=B0,
+        B11c=B11c,
+        B22c=B22c,
+        B20=B20,
+        B22s=B22s,
+        B_theta_20_avg=B_theta_20_avg,
+        len_phi=len_phi,
+        len_phi_axis=500,
+        static_max_freq=(static_freq, static_freq),
+        traced_max_freq=(traced_freq, traced_freq),
+        tol_riccati=1e-8,
+        max_iter_riccati=50,
+        n_shooting_riccati=1000,
+        padded=padded
+    )
+    equilibrium = aqsc.iterate_2(
+        equilibrium,
+        B_denom_nm1 = chiphifunc_type(
+            jnp.array(B_denom_3)[:, None],
+            equilibrium.nfp,
+            trig_mode=True
+        ), # B3
+        B_denom_n = chiphifunc_type(
+            jnp.array(B_denom_4)[:, None],
+            equilibrium.nfp,
+            trig_mode=True
+        ), # B4
+        B_alpha_nb2 = B_alpha_nb2_34,
+        static_max_freq=(static_freq, static_freq),
+        traced_max_freq=(traced_freq, traced_freq),
+    )
+    equilibrium = aqsc.iterate_2(
+        equilibrium,
+        B_denom_nm1 = chiphifunc_type(
+            jnp.array(B_denom_5)[:, None],
+            equilibrium.nfp,
+            trig_mode=True
+        ), # B5
+        B_denom_n = chiphifunc_type(
+            jnp.array(B_denom_6)[:, None],
+            equilibrium.nfp,
+            trig_mode=True
+        ), # B6
+        B_alpha_nb2 = B_alpha_nb2_56,
+        static_max_freq=(static_freq, static_freq),
+        traced_max_freq=(traced_freq, traced_freq),
+    )
+    return equilibrium
+
+
+def divergence_rate(eq):
+    amp_B_psi_coef_cp = eq.unknown['B_psi_coef_cp'].get_l2_order_by_order()
+    amp_B_theta_coef_cp = eq.unknown['B_theta_coef_cp'].get_l2_order_by_order()
+    amp_Delta_coef_cp = eq.unknown['Delta_coef_cp'].get_l2_order_by_order()
+    amp_X_coef_cp = eq.unknown['X_coef_cp'].get_l2_order_by_order()
+    amp_Y_coef_cp = eq.unknown['Y_coef_cp'].get_l2_order_by_order()
+    amp_Z_coef_cp = eq.unknown['Z_coef_cp'].get_l2_order_by_order()
+    amp_p_perp_coef_cp = eq.unknown['p_perp_coef_cp'].get_l2_order_by_order()
+    rate_X_coef_cp = amp_X_coef_cp[-2:] / amp_X_coef_cp[-3:-1]
+    rate_Y_coef_cp = amp_Y_coef_cp[-2:] / amp_Y_coef_cp[-3:-1]
+    rate_Z_coef_cp = amp_Z_coef_cp[-2:] / amp_Z_coef_cp[-3:-1]
+    rate_B_psi_coef_cp = amp_B_psi_coef_cp[-2:] / amp_B_psi_coef_cp[-3:-1]
+    rate_B_theta_coef_cp = amp_B_theta_coef_cp[-2:] / amp_B_theta_coef_cp[-3:-1]
+    rate_Delta_coef_cp = amp_Delta_coef_cp[-2:] / amp_Delta_coef_cp[-3:-1]
+    rate_p_perp_coef_cp = amp_p_perp_coef_cp[-2:] / amp_p_perp_coef_cp[-3:-1]
+    all_rates = jnp.array([
+        rate_X_coef_cp,
+        rate_Y_coef_cp,
+        rate_Z_coef_cp,
+        rate_B_psi_coef_cp,
+        rate_B_theta_coef_cp,
+        rate_Delta_coef_cp,
+        rate_p_perp_coef_cp,
+    ])
+    return jnp.average(all_rates)
+
+def eps_conv(eq):
+    return 1 / divergence_rate(eq)
+
+@jit
+def p_rms_vol(eq, eps, eps2, n_max=float('inf')):
+    vol = eq.volume_eps(eps=eps, n_max=n_max)
+    vol2 = eq.volume_eps(eps=eps2, n_max=n_max)
+    p2 = eq.p_perp * eq.p_perp
+    p2_int = eq.volume_integral(p2, n_max=n_max).eval_eps(eps, 0., 0.)
+    p2_int2 = eq.volume_integral(p2, n_max=n_max).eval_eps(eps2, 0., 0.)
+    p2_int_shell = p2_int - p2_int2
+    vol_shell = vol - vol2
+    p_rms = jnp.sqrt(jnp.real(
+        p2_int_shell/vol_shell
+    ))
+    return p_rms
+
+@jit
+def vol_conv(eq):
+    eps = eps_conv(eq)
+    return eq.volume_eps(eps)
+
+@jit
+def aspect_conv(eq):
+    eps = eps_conv(eq)
+    return eq.aspect_ratio_eps(eps)
+
+@jit
+def aspect_crit(eq):
+    psi, _, _ = eq.get_psi_crit()
+    return eq.aspect_ratio_eps(jnp.sqrt(psi))
+
+@jit
+def rms_anisotropy(eq):
+    eps = eps_conv(eq)
+    int_anisotropy = jnp.real(
+        eq.volume_integral(eq.Delta*eq.Delta).eval_eps(eps=eps, chi=0, phi=0)
+    )
+    return jnp.sqrt(int_anisotropy/vol_conv(eq))
+
+@jit
+def rms_p(eq):
+    eps = eps_conv(eq)
+    int_p_perp = jnp.real(
+        eq.volume_integral(eq.p_perp*eq.p_perp).eval_eps(eps=eps, chi=0, phi=0)
+    )
+    return jnp.sqrt(int_p_perp/vol_conv(eq))
+
+@jit 
+def iota_axis(eq):
+    return jnp.real(eq.iota.eval(psi=0, chi=0, phi=0))

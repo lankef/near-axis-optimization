6f11cc94caf8c57a4a21816ef6ead7429011421d
Lanke Fu <lf2869@torch-login-b-1.hpc-infra.svc.cluster.local>
2026-08-11 11:37:08 -0400

    changing and deleting files

--- shared.py
+++ shared.py
@@ -27,7 +27,7 @@ RZ_clip = 0.3 # fixing min and max R and Z coeffs to prevent crazy axes
 # Targets
 target_iota = 0.1
 target_anisotropy = 1e-2
-target_aspect = 10.
+target_aspect = 25.
 target_beta = 0.05
 population_size = 200
 w_aspect, w_anisotropy, w_iota, w_p = 6., 0.5, 1., 1.

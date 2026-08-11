0395f6f4da02fe37ce37d40f590f2c49d24805e6
Lanke Fu <lf2869@torch-login-b-1.hpc-infra.svc.cluster.local>
2026-08-11 08:25:05 -0400

    data

--- shared.py
+++ shared.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import jax
 import jax.numpy as jnp
 
-jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
+jax.config.update("jax_compilation_cache_dir", "./tmp/jax_cache")
 jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
 jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
 jax.config.update("jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir")
@@ -31,7 +31,7 @@ target_aspect = 10.
 target_beta = 0.05
 population_size = 200
 w_aspect, w_anisotropy, w_iota, w_p = 6., 0.5, 1., 1.
-std_init = 4.0
+std_init = 0.2
 
 
 def spec_weight(n):

0adb2bfe3898c8993682663a553f171325db596a
Lanke Fu <lf2869@torch-login-a-2.hpc-infra.svc.cluster.local>
2026-08-28 14:51:55 -0400

    data

---
+++ jobscript_scan.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH --account=torch_pr_292_courant
+#SBATCH --cpus-per-task=4
+#SBATCH --time=00:20:00
+#SBATCH --output=logs/slurm_%A.out
+#SBATCH --error=logs/slurm_%A.err
+#SBATCH --mem=30G
+#SBATCH --gres=gpu:l40s:1
+
+# Pick the script for this array index
+
+module load anaconda3/2025.06
+source $(conda info --base)/etc/profile.d/conda.sh
+
+conda activate desc
+mkdir -p population_eq
+mkdir -p global_steps
+python -u scan_population.py
